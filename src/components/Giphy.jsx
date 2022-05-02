416ac400c38845ad4eeaf50e78cccf9bdb14c128
Ryan Kuhn <ryankuhn@ryans-mbp.lan>
2022-05-01 20:53:50 -0400

    A simple application that makes an API call and returns a gif to the user, user can also search GIF's

---
+++ src/components/Giphy.jsx
@@ -0,0 +1,99 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios"
+import Loader from '../Loader';
+
+const Giphy = () => {
+
+    const[data, setData] = useState([]);
+    const[search, setSearch] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+
+    useEffect (() => {
+        const fetchData = async () => {
+            setIsError(false)
+            setIsLoading(true);
+            try {
+                const results = await axios("https://api.giphy.com/v1/gifs/trending", { 
+                    params: {
+                    api_key: "8jEkOv0ckmrH7zN2ljn7ATajJlkQ1n3K"
+                }
+            });      
+
+            console.log(results)
+            setData(results.data.data);
+
+            } catch(err) {
+                setIsError(true)
+                setTimeout(() => setIsError(false), 4000)       
+            }
+     
+    
+    setIsLoading(false)
+};
+    fetchData();
+}, []);
+
+const renderGifs = () => {
+    if(isLoading) {
+        return <Loader />
+    }
+    return data.map(el => {
+        return (
+            <div key={el.id} className="gif">
+        <img src={el.images.fixed_height.url}/>
+        </div>
+        );
+    });
+};
+
+const renderError = () => {
+    if(isError) {
+        return (
+            <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                Unable to get Gifs, please try again in a few minutes.
+            </div>
+        );
+    }
+};
+
+const handleSearchChange = event => {
+    setSearch(event.target.value);
+};
+
+const handleSubmit = async event => {
+    event.preventDefault();
+    setIsError(false)
+    setIsLoading(true)
+    try {
+        const results = await axios("https://api.giphy.com/v1/gifs/search", {
+    params: {
+        api_key: "8jEkOv0ckmrH7zN2ljn7ATajJlkQ1n3K",
+        q: search
+    }
+    });
+    setData(results.data.data);
+    } catch (err) {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 4000);
+    }
+    
+    setIsLoading(false)
+};
+  
+    return (
+        <div className='m-2'>
+            {renderError()}
+            <form className='form-inline justify-content-center m-2'>
+                <input onChange={handleSearchChange} type="text" placeholder="search" className="form-control" />
+                <button onClick={handleSubmit} type='submit' className='btn btn-primary mx-2'>Go</button>
+            </form>
+         <div className="container gifs">
+             {renderGifs()}
+             </div>
+        </div>     
+    )
+};
+    
+
+export default Giphy;
